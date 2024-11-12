79af4eb287588b396f966236ab2b5774e3fc4286
reylawas <127361912+reylawas@users.noreply.github.com>
2024-11-12 22:12:39 +0800

    Update login.php

--- employee/login.php
+++ employee/login.php
@@ -19,11 +19,11 @@ if (isset($_POST['login'])) {
             
             // Check if the logged-in user has the position of Admin
             if ($row['position'] === 'Administrative Officer II') {
-                header('Location: http://localhost/epms/admin/home.php');
+                header('Location: https://c084kw48cwow40ccoggkkg8k.143.198.91.47.sslip.io/admin/home.php');
             } 
             // Check if the logged-in user has the position of School Head
             else if ($row['position'] === 'School Head') {
-                header('Location: http://localhost/epms/school_head/home.php');
+                header('Location: https://c084kw48cwow40ccoggkkg8k.143.198.91.47.sslip.io/school_head/home.php');
             } 
             else {
                 header('Location: index.php'); // Redirect non-admin users to the main page
