9bf25b91c302b27bd485c4afc985e310bc86f2fb
reylawas <ryelaswa@gmail.com>
2024-11-12 16:02:28 +0800

    Updated repository with latest changes made to React application on YYYY-MM-DD

--- employee/includes/conn.php
+++ employee/includes/conn.php
@@ -1,8 +1,13 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'apsystem');
+	$host = getenv('DB_HOST');
+	$database = getenv('DB_DATABASE');
+	$username = getenv('DB_USERNAME');
+	$password = getenv('DB_PASSWORD');
+	$port = getenv('DB_PORT');
+
+	$conn = new mysqli($host, $username, $password, $database, $port);
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
-	
 ?>
